5b50ea8918aa4f61b2b00841ca3caa51f0c8d5af
luntuk <152620332+luntuk@users.noreply.github.com>
2026-04-09 14:22:28 +0300

    Create constants.js

---
+++ utils/constants.js
@@ -0,0 +1,28 @@
+export const JOB_STATUS = {
+  PENDING: 'pending',
+  INTERVIEW: 'interview',
+  DECLINED: 'declined',
+};
+
+export const JOB_TYPE = {
+  FULL_TIME: 'full-time',
+  PART_TIME: 'part-time',
+  REMOTE: 'remote',
+};
+
+export const JOB_SORT_BY = {
+  NEWEST_FIRST: 'newest',
+  OLDEST_FIRST: 'oldest',
+  ASCENDING: 'a-z',
+  DESCENDING: 'z-a',
+};
+
+export const TEST_USER_ID = '64b2c07ccac2efc972ab0eca';
+
+export const DEMO_USER = {
+  email: 'demo@example.com',
+  password: 'demo123456',
+  _id: '999999999999999999999999',
+  name: 'Demo User',
+  role: 'user'
+};
